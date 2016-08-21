03d0617bbeb18a9eb0f33e76e6299c1e2c2f2eaa
sdsawtelle <sdsawtelle@gmail.com>
2016-08-21 16:47:38 -0400

    blog test

--- datablog/pelicanconf.py
+++ datablog/pelicanconf.py
@@ -34,6 +34,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATH = './plugins'
