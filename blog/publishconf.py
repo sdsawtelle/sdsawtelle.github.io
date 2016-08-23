2731f7e0d1e73cfeb4d6082320d01c1b9affeee4
sdsawtelle <sdsawtelle@gmail.com>
2016-08-23 12:46:21 -0400

    tinkering with pelican publishing settings

--- blog/publishconf.py
+++ blog/publishconf.py
@@ -13,8 +13,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
-RELATIVE_URLS = False
+SITEURL = 'sdsawtelle.github.io/blog'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
